c3c40899178a4fba73fe2542dbe8f4e48cbbe680
Emily Asch <e.asch@me.com>
2018-10-18 18:55:34 +0000

    automatically backed up by learn

--- tweet_shortener.rb
+++ tweet_shortener.rb
@@ -14,7 +14,7 @@ def dictionary
   }
 end
 
-def word_substituter(tweet)
+def word_substituter(tweet_one)
   tweet.split.collect {|word| dictionary.keys.include? (word.downcase) ? word = dictionary[word.downcase] : word}.join(" ")
 end
 
